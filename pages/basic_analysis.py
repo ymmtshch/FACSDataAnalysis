225c309e0aaba349fc1868443b91846ee664858b
ymmtshch <shoichi.yamamoto@konicaminolta.com>
2025-06-18 16:39:27 +0900

    Update basic_analysis.py

--- pages/basic_analysis.py
+++ pages/basic_analysis.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import fcsparser
 from utils.fcs_processor import FCSProcessor
-from utils.plotting import PlottingUtils
+from utils.plotting import PlottingUtils  # 新しく追加したクラスを使用
 import plotly.express as px
 import plotly.graph_objects as go
 from config import Config
