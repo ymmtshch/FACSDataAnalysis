b7630e98eb906eb547fac2957fdcbd259e404284
ymmtshch <shoichi.yamamoto@konicaminolta.com>
2025-09-19 14:34:49 +0900

    Update test_fcs_processor.py

--- tests/test_fcs_processor.py
+++ tests/test_fcs_processor.py
@@ -1,1 +1,12 @@
+import pytest
+import pandas as pd
+from utils import fcs_processor
 
+def test_fcs_processor_initialization():
+    processor = fcs_processor.FCSProcessor(pd.DataFrame())
+    assert processor is not None
+
+def test_export_data_empty():
+    processor = fcs_processor.FCSProcessor(pd.DataFrame())
+    csv = processor.export_data(pd.DataFrame(), data_type="data")
+    assert isinstance(csv, str)
